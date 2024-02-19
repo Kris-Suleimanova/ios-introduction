7b38116bb51dfe6bcd6e2c8c43fb0298a5a06267
Kris-Suleimanova <112935778+Kris-Suleimanova@users.noreply.github.com>
2024-02-19 12:40:42 +0300

    Add files via upload

---
+++ Введение.swift
@@ -0,0 +1,7 @@
+let first _name: String = "Кристина"
+let last_name: String = "Сулейманова"
+let university: String = "ETU"
+print("Name:",first _name)
+print("last_name:", last_name)
+print("university:",  university)
+
